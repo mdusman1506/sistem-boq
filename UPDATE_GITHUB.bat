51114a1e64c7b584ed7907bda410223057b0aaca
mdusman1506 <usmanlost.saga@gmail.com>
2026-06-17 22:35:39 +0700

    Fix: update controller, perbaikan bug CCO, bersihkan laporan

--- UPDATE_GITHUB.bat
+++ UPDATE_GITHUB.bat
@@ -6,6 +6,7 @@ echo.
 echo Mengupdate file yang berubah ke GitHub...
 "C:\Users\usman\AppData\Local\GitHubDesktop\app-3.5.8\resources\app\git\cmd\git.exe" add .
 "C:\Users\usman\AppData\Local\GitHubDesktop\app-3.5.8\resources\app\git\cmd\git.exe" commit -m "Fix: update controller, perbaikan bug CCO, bersihkan laporan"
+"C:\Users\usman\AppData\Local\GitHubDesktop\app-3.5.8\resources\app\git\cmd\git.exe" pull origin main --rebase
 "C:\Users\usman\AppData\Local\GitHubDesktop\app-3.5.8\resources\app\git\cmd\git.exe" push origin main
 echo.
 echo ====================================================
